ef735454f764a1d0b162360a33a7b979d94691ef
NULLICN <121741693+NULLICN@users.noreply.github.com>
2026-07-15 23:49:00 +0800

    优化接口：添加到购物车

--- sky-take-server/src/main/java/xyz/nullicn/skytakeserver/service/impl/ShoppingCartServiceImpl.java
+++ sky-take-server/src/main/java/xyz/nullicn/skytakeserver/service/impl/ShoppingCartServiceImpl.java
@@ -1,6 +1,5 @@
 package xyz.nullicn.skytakeserver.service.impl;
 
-import cn.hutool.ai.core.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,35 +9,27 @@ import xyz.nullicn.dto.ShoppingCartDTO;
 import xyz.nullicn.entity.Dish;
 import xyz.nullicn.entity.Setmeal;
 import xyz.nullicn.entity.ShoppingCart;
-import xyz.nullicn.enumeration.OperationType;
-import xyz.nullicn.skytakeserver.annotation.AutoFill;
+import xyz.nullicn.exception.BaseException;
 import xyz.nullicn.skytakeserver.mapper.DishMapper;
 import xyz.nullicn.skytakeserver.mapper.SetmealMapper;
 import xyz.nullicn.skytakeserver.mapper.ShoppingCartMapper;
-import xyz.nullicn.skytakeserver.service.DishService;
-import xyz.nullicn.skytakeserver.service.SetmealService;
 import xyz.nullicn.skytakeserver.service.ShoppingCartService;
-import xyz.nullicn.utils.JwtUtil;
 
 @Service
 public class ShoppingCartServiceImpl implements ShoppingCartService {
 
     @Autowired
-    DishMapper dishMapper;
-
+    private DishMapper dishMapper;
     @Autowired
-    SetmealMapper setmealMapper;
-
+    private SetmealMapper setmealMapper;
     @Autowired
-    ShoppingCartMapper shoppingCartMapper;
+    private ShoppingCartMapper shoppingCartMapper;
 
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void addToCart(ShoppingCartDTO shoppingCartDTO) {
-        // 通过jwt校验得到用户id
         Long userId = BaseContext.getCurrentId();
 
-        // 判断本次是添加菜品还是套餐
         Long dishId = shoppingCartDTO.getDishId();
         Long setmealId = shoppingCartDTO.getSetmealId();
 
@@ -46,52 +37,38 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         boolean hasSetmeal = setmealId != null && setmealId > 0;
 
         if (hasDish && hasSetmeal) {
-            throw new RuntimeException(MessageConstant.DISH_SETMEAL_INCOMPATIBLE);
+            throw new BaseException(MessageConstant.DISH_SETMEAL_INCOMPATIBLE);
         }
         if (!hasDish && !hasSetmeal) {
-            throw new RuntimeException(MessageConstant.DISH_SETMEAL_EMPTY);
+            throw new BaseException(MessageConstant.DISH_SETMEAL_EMPTY);
         }
 
-        // 查询对应菜品或套餐其余数据
-        Dish dish = null;
-        Setmeal setmeal = null;
+        ShoppingCart.ShoppingCartBuilder builder = ShoppingCart.builder()
+                .userId(userId)
+                .number(1);
 
-        if(hasDish) {
-            dish = dishMapper.getById(dishId);
-            if(dish == null) {
-                throw new RuntimeException(MessageConstant.DISH_NOT_EXISTS);
+        if (hasDish) {
+            Dish dish = dishMapper.getById(dishId);
+            if (dish == null) {
+                throw new BaseException(MessageConstant.DISH_NOT_EXISTS);
             }
-        }
-
-        if(hasSetmeal) {
-            setmeal = setmealMapper.getById(setmealId);
-            if(setmeal == null) {
-                throw new RuntimeException(MessageConstant.SETMEAL_NOT_EXISTS);
+            builder.name(dish.getName())
+                   .dishId(dishId)
+                   .dishFlavor(shoppingCartDTO.getDishFlavor() != null
+                           ? shoppingCartDTO.getDishFlavor() : "")
+                   .amount(dish.getPrice())
+                   .image(dish.getImage());
+        } else {
+            Setmeal setmeal = setmealMapper.getById(setmealId);
+            if (setmeal == null) {
+                throw new BaseException(MessageConstant.SETMEAL_NOT_EXISTS);
             }
+            builder.name(setmeal.getName())
+                   .setmealId(setmealId)
+                   .amount(setmeal.getPrice())
+                   .image(setmeal.getImage());
         }
 
-        // 构建ShoppingCart对象并写入数据库
-        ShoppingCart goods = new ShoppingCart();
-        goods.setUserId(userId);
-
-
-        if (dish != null) {
-            goods.setName(dish.getName());
-            goods.setDishId(dishId);
-            goods.setDishFlavor(shoppingCartDTO.getDishFlavor());
-            goods.setNumber(1);
-            goods.setAmount(dish.getPrice());
-            goods.setImage(dish.getImage());
-        }
-
-        if (setmeal != null) {
-            goods.setName(setmeal.getName());
-            goods.setDishId(setmealId);
-            goods.setNumber(1);
-            goods.setAmount(setmeal.getPrice());
-            goods.setImage(setmeal.getImage());
-        }
-
-        shoppingCartMapper.insert(goods);
+        shoppingCartMapper.insert(builder.build());
     }
 }
